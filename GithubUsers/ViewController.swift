ab280ac4b10c393b7de28084ca5a1d011d63ca8d
Kevin Siundu <kevin.siundu@equitybank.co.ke>
2021-05-07 19:41:29 +0300

    Initial Commit

---
+++ GithubUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUsers
+//
+//  Created by Kevin Siundu on 07/05/2021.
+//  Copyright © 2021 Kevin Siundu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
